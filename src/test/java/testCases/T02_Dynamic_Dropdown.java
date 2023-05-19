db191bdf6278454b16ca4e3e71c2c304f73b9c9f
MoustafaSWE <moustafa.mo.ismail@gmail.com>
2023-05-19 15:59:27 +0200

    update project

--- src/test/java/testCases/T02_Dynamic_Dropdown.java
+++ src/test/java/testCases/T02_Dynamic_Dropdown.java
@@ -1,6 +1,7 @@
 package testCases;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -26,13 +27,11 @@ public class T02_Dynamic_Dropdown {
 
         // 2- Create new object from chromedriver
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver();
+
         // config
-        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
         // 3- Configuration
         //3.1- Maximize browser
@@ -53,10 +52,11 @@ public class T02_Dynamic_Dropdown {
     public void dynamicDropdown() throws InterruptedException {
 
         //1- search on "selenium"
+        driver.findElement(By.name("q")).sendKeys("selenium");
 
         // 2- dynamic dropdown list
         // We couldn't use Select class with dynamic lists, instead we could handle it with findElements() method
-
+        driver.findElements(By.cssSelector("ul[jsname=\"bw4e9b\"] li")).get(2).click();
 
     }
 
