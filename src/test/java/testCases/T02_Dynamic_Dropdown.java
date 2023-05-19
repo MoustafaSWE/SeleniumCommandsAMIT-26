64758c504af188f7a06ce92121a0c85b3937419b
MoustafaSWE <moustafa.mo.ismail@gmail.com>
2023-05-19 16:17:01 +0200

    update project

--- src/test/java/testCases/T02_Dynamic_Dropdown.java
+++ src/test/java/testCases/T02_Dynamic_Dropdown.java
@@ -3,13 +3,17 @@ package testCases;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
 
 public class T02_Dynamic_Dropdown {
 
@@ -54,8 +58,15 @@ public class T02_Dynamic_Dropdown {
         //1- search on "selenium"
         driver.findElement(By.name("q")).sendKeys("selenium");
 
+
         // 2- dynamic dropdown list
         // We couldn't use Select class with dynamic lists, instead we could handle it with findElements() method
+
+        // Explicit wait
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("ul[jsname=\"bw4e9b\"] li")));
+
         driver.findElements(By.cssSelector("ul[jsname=\"bw4e9b\"] li")).get(2).click();
 
     }
